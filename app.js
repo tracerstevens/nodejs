2855b291b5ffdb87278d88ace7b88917ddb27e4d
= <=>
2014-09-06 10:06:08 -0700

    added cluster console commands

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var cluster = require('cluster');
 var restify = require('restify');
 var bson = require('bson');
 var util = require('util');
@@ -6,6 +7,8 @@ var cors = require('cors');
 
 var MongoClient = require('mongodb').MongoClient;
 var db;
+
+var numCPUs = require('os').cpus().length;
 
 
 /**
@@ -17,41 +20,60 @@ MongoClient.connect("mongodb://127.0.0.1:27017/exampleDb", function(err, databas
   db = database;
 });
 
-
 /**
- * Server
- * @type {*}
+ * Multi Core Ability
  */
-var server = restify.createServer();
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-server.use(cors());
-
-
-/**
- * Routes
- */
-server.get('/message/:id', function(req, res, next) {
-  db.collection("test").find({user_id : 10}).toArray(function(err,items){
-    res.send(items);
+  cluster.on('online', function(worker) {
+    console.log('worker ' + worker.process.pid + ' online');
   });
 
-  return next();
-});
-
-server.post('/messages', function(req, res, next) {
-  db.collection("test").insert({user_id : 10, message : req.params.message}, function(err, result) {});
-
-  res.send("Insert Complete");
-  return next();
-});
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
 
 
-/**
- * Listen
- */
-server.listen(3000, function() {
-	console.log('%s listening at %s', server.name, server.url);
-});
+  /**
+   * Server
+   * @type {*}
+   */
+  var server = restify.createServer();
+
+  server.use(restify.acceptParser(server.acceptable));
+  server.use(restify.queryParser());
+  server.use(restify.bodyParser());
+  server.use(cors());
+
+
+  /**
+   * Routes
+   */
+  server.get('/message/:id', function(req, res, next) {
+    console.log('Request on worker #' + cluster.worker.id);
+    db.collection("test").find({user_id : 10}).toArray(function(err,items){
+      res.send(items);
+    });
+
+    return next();
+  });
+
+  server.post('/messages', function(req, res, next) {
+    db.collection("test").insert({user_id : 10, message : req.params.message}, function(err, result) {});
+
+    res.send("Insert Complete");
+    return next();
+  });
+
+  /**
+   * Listen
+   */
+  server.listen(3000, function() {
+    console.log('%s listening at %s', server.name, server.url);
+  });
+}
